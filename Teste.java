cc515d4e5a8a32b9f3c323a98b7b07bc1dcda3cb
Daniel Teixeira <danieltl@poli.ufrj.br>
2020-01-06 13:57:50 -0300

    Questao 1 teste

---
+++ Teste.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package processo_seletivo_radix;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Daniel
+ */
+public class Questao1 {
+     public static void main(String[] args) {
+       Scanner teclado = new Scanner(System.in);
+        System.out.println("Digite o termo: ");
+       int termo = teclado.nextInt();
+        System.out.println(termo);
+     }   
+}
